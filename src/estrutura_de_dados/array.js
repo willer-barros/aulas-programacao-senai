beddf47785c01beb73c6d7c4ca2ef9c0bdd55922
willer <willerbarros137@gmail.com>
2026-04-18 18:49:21 -0300

    array

---
+++ src/estrutura_de_dados/array.js
@@ -0,0 +1,22 @@
+/*os arrays(vatores) sao estruturas de dados versateis
+aceitam varios tipos de dados
+como:
+################################
+String
+const nomes = ["Tadeu", "Santana", "Inacio"]
+
+
+################################
+Numerico
+const nomes = [8, 7, 8]
+
+################################
+Misto
+const nomes = [8, "willer", 5]
+
+*/
+
+
+const nomes = [8, "Santana", "Inacio"]
+
+console.log(nomes)
