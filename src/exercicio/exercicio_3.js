170e46591f9063c4946b08043134f302cd585f08
willer-barros <willerbarros137@gmail.com>
2026-04-27 19:22:25 -0300

    exercicios 4 e 5

--- src/exercicio/exercicio_3.js
+++ src/exercicio/exercicio_3.js
@@ -1,15 +1,4 @@
 // Use o módulo nativo fs para criar um arquivo chamado mensagem.txt e escrever o texto "Node.js é poderoso!" dentro dele.
 //para usar esse modulo, usamos o metodo require do node. ficando assim const fs = require("fs")
 
-const fs = require("fs")
-
-const texto = "Nodejs é poderoso"
-
-fs.writeFile("mensagem.txt", texto, (error) =>{
-    if(error){
-        console.error("Erro na criacao do arquivo", error);
-        return
-    }
-    console.log("O Arquivo foi criado com sucesso")
-})
 
