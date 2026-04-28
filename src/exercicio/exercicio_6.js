17f7187f3870124aa080c7718f391fd3e15547fc
willer-barros <willerbarros137@gmail.com>
2026-04-27 21:26:02 -0300

    10 exercicios criados para resolucao

---
+++ src/exercicio/exercicio_6.js
@@ -0,0 +1,3 @@
+// Use o módulo http para criar um servidor básico que rode na porta 3000 e responda "Bem-vindo ao meu servidor Node!" para qualquer requisição.HTTP	Use o 
+// módulo http para criar um servidor básico que rode na porta 3000 e responda "Bem-vindo ao meu servidor Node!" para qualquer requisição.
+// nessa etapa nos começaremos a falar de desenvolvimento web
