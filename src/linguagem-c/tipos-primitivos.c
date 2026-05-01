4cdd9a11f70f7172714e8e612a13262d7d8059e8
willer-barros <willerbarros137@gmail.com>
2026-05-01 14:49:13 -0300

    create linguagem-c

---
+++ src/linguagem-c/tipos-primitivos.c
@@ -0,0 +1,7 @@
+// Tipos Primitivos em C
+// Tipo	Descrição	Exemplo de Declaração
+// char	Caractere (1 byte)	char letra = 'A';
+// int	Número inteiro	int idade = 25;
+// float	Número real (precisão simples)	float nota = 8.5f;
+// double	Número real (precisão dupla)	double pi = 3.14159;
+// void	Ausência de tipo	Usado em funções e ponteiros
