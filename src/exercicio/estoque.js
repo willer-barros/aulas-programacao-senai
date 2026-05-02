32167f8594b9138505bd8871c155b6fe88f26f3a
willer-barros <willerbarros137@gmail.com>
2026-05-02 19:35:33 -0300

    atividade estoque

---
+++ src/exercicio/estoque.js
@@ -0,0 +1,8 @@
+const estoque = [
+    {"nome": "Motor V8", "categoria": "A"},
+    {"nome": "Amortecedor", "categoria": "A"},
+    {"nome": "Freio ABC", "categoria": "B"},
+    {"nome": "Correia Dentada", "categoria": "C"},
+]
+
+
