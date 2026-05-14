51a27021c82ea006d15eb513732048004a9a9098
victoroliveira6-ops <victor_oliveira6@estudante.sesisenai.org.br>
2026-05-14 08:04:30 -0300

    feat: exercícios 6 ao 10

--- src/exercicio/exercicio_10.js
+++ src/exercicio/exercicio_10.js
@@ -1,30 +1,41 @@
 // 1. Variáveis e Dados (Array de Objetos)
+
 const logsDeAcesso = [
   { usuario: "admin", status: "sucesso" },
   { usuario: "willer_barros_senai", status: "erro" },
   { usuario: "arthur_basilio_senai", status: "erro" },
   { usuario: "neymar junior", status: "erro" },
   { usuario: "kallany_gretchen_senai", status: "sucesso" }
-];
+]
 
-let totalErros = 0;
+let totalErros = 0
 
-console.log("--- Iniciando Análise de Logs ---");
+console.log("--- Iniciando Análise de Logs ---")
 
 for (const log of logsDeAcesso) {
+
   if (log.status === "erro") {
-    console.log(`ALERTA: Falha de acesso detectada para o usuário: ${log.usuario}`);
-    totalErros = totalErros + 1;
+
+    console.log(`ALERTA: Falha de acesso detectada para o usuário: ${log.usuario}`)
+
+    totalErros = totalErros + 1
+
   } else {
-    console.log(`Acesso normal: ${log.usuario}`);
+
+    console.log(`Acesso normal: ${log.usuario}`)
+
   }
 }
 
-console.log("---------------------------------");
-console.log(`Total de falhas encontradas: ${totalErros}`);
+console.log("---------------------------------")
+console.log(`Total de falhas encontradas: ${totalErros}`)
 
 if (totalErros > 2) {
-  console.log("STATUS DO SISTEMA: Bloqueio preventivo ativado! (Muitas falhas)");
+
+  console.log("STATUS DO SISTEMA: Bloqueio preventivo ativado! (Muitas falhas)")
+
 } else {
-  console.log("STATUS DO SISTEMA: Operando normalmente.");
+
+  console.log("STATUS DO SISTEMA: Operando normalmente.")
+
 }
