0be1122e61586052d35eb208c3bc997672566acd
victoroliveira6-ops <victor_oliveira6@estudante.sesisenai.org.br>
2026-05-14 07:52:19 -0300

    feat: exercícios 1 ao 5

--- src/exercicio/exercicio_1.js
+++ src/exercicio/exercicio_1.js
@@ -2,6 +2,7 @@
 // Crie um script que receba dois números 
 // (podem ser variáveis fixas por enquanto) e exiba a soma, subtração, multiplicação e divisão entre eles.
 
+
 let numero_1 = 10
 let numero_2 = 10
 
@@ -9,4 +10,5 @@ console.log("Soma: " + (numero_1 + numero_2))
 console.log("Subtração: " + (numero_1 - numero_2))
 console.log("Multiplicação: " + (numero_1 * numero_2))
 console.log("Divisão: " + (numero_1 / numero_2))
+
 
