0c4312e963c1d215608b62f85096951eefed83cb
victoroliveira6-ops <victor_oliveira6@estudante.sesisenai.org.br>
2026-05-15 12:05:59 -0300

    sistema_lista

--- src/projects/sistema_lista/script.js
+++ src/projects/sistema_lista/script.js
@@ -19,9 +19,48 @@ document.getElementById('btn-enviar').addEventListener('click', cadastrar);
 async function atualizarLista() {
   const res = await fetch('http://localhost:3000/alunos');
   const dados = await res.json();
-  
+
   const lista = document.getElementById('lista');
-  lista.innerHTML = dados.map(a => `<li>${a.nome}</li>`).join('');
+
+  lista.innerHTML = dados.map(a => `
+    <li>
+      ${a.nome}
+
+      <button onclick="editarAluno(${a.id})">
+        Editar
+      </button>
+
+      <button onclick="deletarAluno(${a.id})">
+        Excluir
+      </button>
+    </li>
+  `).join('');
+}
+
+async function deletarAluno(id) {
+  await fetch(`http://localhost:3000/alunos/${id}`, {
+    method: 'DELETE'
+  });
+
+  atualizarLista();
+}
+
+async function editarAluno(id) {
+  const novoNome = prompt("Digite o novo nome:");
+
+  if (!novoNome) return;
+
+  await fetch(`http://localhost:3000/alunos/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      nome: novoNome
+    })
+  });
+
+  atualizarLista(); 
 }
 
 // Carregar ao abrir a página
