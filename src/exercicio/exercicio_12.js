2d6e7af37d2c2b49e75d58e26ed92d00b3486a21
Patrícia Nobre <nobre2008mm@gmail.com>
2026-05-17 12:45:16 -0300

    Substitui apenas a pasta src/exercicio pela versao da main

---
+++ src/exercicio/exercicio_12.js
@@ -0,0 +1,4 @@
+// 2. Verificar Presença e Índice com includes() e indexOf()
+// Verifique se um número está presente em um array e retorne sua posição. 
+
+const numeros = [10, 20, 30, 40, 50];
--- src/exercicio/exercicio_2.js
+++ src/exercicio/exercicio_2.js
@@ -3,3 +3,11 @@
 
 const num = 1442
 
+if (num % 2 ===  0) {
+    console.log("O numero " + num + " é par")
+} else {
+    console.log("O numero " + num + " é impar")
+}
+
+
+
