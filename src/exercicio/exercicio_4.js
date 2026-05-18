87327eab9c9fd20fe7a1bc8cdbc65b8656610440
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:37:57 -0300

    Resolução dos exercícios

--- src/exercicio/exercicio_4.js
+++ src/exercicio/exercicio_4.js
@@ -1,4 +1,12 @@
 // Utilize o módulo fs para ler o conteúdo do arquivo mensagem.txt criado no exercício anterior e exibi-lo no terminal.
 //voce ja aprendeu no exercicio anterior como faz a importacao do modulo fs
 
+const fs = require("fs")
 
+fs.readFile("mensagem.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log("Erro ao ler o arquivo")
+    } else {
+        console.log(data)
+    }
+})
