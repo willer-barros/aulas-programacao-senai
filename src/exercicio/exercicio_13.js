1e6a410f3f53a4542fcfac538c20b8560dfc38d6
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:53:39 -0300

    Resolução exercício forEach

--- src/exercicio/exercicio_13.js
+++ src/exercicio/exercicio_13.js
@@ -1,4 +1,12 @@
 // 3. Iterar com forEach() e Aplicar Operações
-// Use forEach() para percorrer um array de preços e aplicar um desconto de 10% em cada item. 
 
-const precos = [100, 250, 80, 120];
+const precos = [100, 250, 80, 120]
+
+precos.forEach((preco) => {
+
+    const desconto = preco * 0.10
+    const precoFinal = preco - desconto
+
+    console.log(`Preço original: R$${preco}`)
+    console.log(`Preço com desconto: R$${precoFinal}`)
+})
