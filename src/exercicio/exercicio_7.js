75dc8a4a566289904fa200e6eb9f34eb04c821a5
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:42:02 -0300

    Resolução exercício filter

--- src/exercicio/exercicio_7.js
+++ src/exercicio/exercicio_7.js
@@ -1,1 +1,13 @@
 // Dado um array de objetos de "produtos" (nome e preço), use o método .filter() para exibir apenas os produtos que custam mais de R$ 50,00.
+
+const produtos = [
+    { nome: "Teclado", preco: 120 },
+    { nome: "Mouse", preco: 40 },
+    { nome: "Monitor", preco: 900 },
+    { nome: "Caderno", preco: 25 },
+    { nome: "Headset", preco: 80 }
+]
+
+const produtosFiltrados = produtos.filter(produto => produto.preco > 50)
+
+console.log(produtosFiltrados)
