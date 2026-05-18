b04399c803410a77940489a7ccb349a4359e67ad
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:42:33 -0300

    Resolução exercício path

--- src/exercicio/exercicio_8.js
+++ src/exercicio/exercicio_8.js
@@ -1,1 +1,10 @@
 // Caminhos	Use o módulo path para exibir a extensão de um nome de arquivo fictício (ex: "imagem.png") e o caminho absoluto do diretório atual (__dirname).
+
+const path = require("path")
+
+const arquivo = "imagem.png"
+
+const extensao = path.extname(arquivo)
+
+console.log("Extensão do arquivo:", extensao)
+console.log("Diretório atual:", __dirname)
