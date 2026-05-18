66f92c5e730719d32c4092e0429d3f36bd62173f
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:45:26 -0300

    Resolução exercício fila veterinária

--- src/exercicio/exercicio_11.js
+++ src/exercicio/exercicio_11.js
@@ -2,4 +2,18 @@
 // Simule uma fila de atendimento em uma clínica veterinária, onde os animais são atendidos na ordem de chegada.
 
 // Cria um array vazio para representar a fila
-const clinica = [];
+
+const clinica = []
+
+// Adicionando animais na fila
+clinica.push("Rex")
+clinica.push("Mingau")
+clinica.push("Bolt")
+
+console.log("Fila atual:", clinica)
+
+// Atendendo o primeiro animal da fila
+const animalAtendido = clinica.shift()
+
+console.log("Animal atendido:", animalAtendido)
+console.log("Fila restante:", clinica)
