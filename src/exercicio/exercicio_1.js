102a9a55a58d4673abbe78de2a9e601e00e12b4c
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:34:50 -0300

    Resolução dos exercícios

--- src/exercicio/exercicio_1.js
+++ src/exercicio/exercicio_1.js
@@ -5,8 +5,13 @@
 let numero_1 = 10
 let numero_2 = 10
 
-console.log("Soma: " + (numero_1 + numero_2))
-console.log("Subtração: " + (numero_1 - numero_2))
-console.log("Multiplicação: " + (numero_1 * numero_2))
-console.log("Divisão: " + (numero_1 / numero_2))
+let soma = numero_1 + numero_2
+let subtracao = numero_1 - numero_2
+let multiplicacao = numero_1 * numero_2
+let divisao = numero_1 / numero_2
+
+console.log("Soma:", soma)
+console.log("Subtração:", subtracao)
+console.log("Multiplicação:", multiplicacao)
+console.log("Divisão:", divisao)
 
