673a42285b5619684b40944d1438708494576217
Victor Dias <victordiasq12@gmail.com>
2026-05-17 21:54:09 -0300

    Resolução exercício catálogo de produtos

--- src/exercicio/exercicio_14.js
+++ src/exercicio/exercicio_14.js
@@ -1,8 +1,15 @@
-// Simule um catálogo de produtos onde cada chave é o nome do produto e o valor é o preço. 
+// Simule um catálogo de produtos onde cada chave é o nome do produto e o valor é o preço.
 
 // Cria um dicionário (objeto) de produtos
 const produtos = {
-    'Camiseta': 49.90,
-    'Calça': 99.90,
-    'Tênis': 199.90
-};
+    Camiseta: 49.90,
+    Calça: 99.90,
+    Tênis: 199.90
+}
+
+// Exibindo os produtos
+for (const produto in produtos) {
+
+    console.log(`${produto}: R$ ${produtos[produto]}`)
+
+}
